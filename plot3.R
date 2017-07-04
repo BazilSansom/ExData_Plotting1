ab08e71e928061025bd73508ed6fbac387c4d2ff
BazilSansom <bazilsansom@gmail.com>
2017-07-04 23:54:53 +0100

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -6,10 +6,10 @@
 ## Reading and cleaning data
 data=read.csv("household_power_consumption.txt", na.strings="?", sep=";", stringsAsFactors=F) #load dataset from working directory
 data$Date <- as.Date(data$Date, format="%d/%m/%Y") #Format Date from chr to date
-datatime <- as.POSIXct(paste(data$Date, data$Time), format="%Y-%m-%d %H:%M:%S") # Form and format date-time variable
+datetime <- as.POSIXct(paste(data$Date, data$Time), format="%Y-%m-%d %H:%M:%S") # Form and format date-time variable
 data$Time <- NULL # Ditch now redundant Time variable
 data$Date <- NULL # Ditch now redundant Date variable
-data <- subset(data, Date > "2007-02-01 00:00:01" & Date < "2007-02-02 24:00:00") # Subset data for data from the dates 2007-02-01 and 2007-02-02 period of interest
+data <- subset(data, datetime > "2007-02-01 00:00:01" & datetime < "2007-02-02 24:00:00") # Subset data for data from the dates 2007-02-01 and 2007-02-02 period of interest
 
 ## Creating plots
 
